56d3b46ce0113cc6daa3392a6a971ed490759f3a
Halimat Shaibu <tamilahshaibu@gmail.com>
2024-05-23 22:37:36 +0100

    Update Bookstore.sol

--- Bookstore.sol
+++ Bookstore.sol
@@ -1,73 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract MyBookStore {
-    //address of the owner of the contract
+contract BookStore{
+    //a state variable of the address of the owner
     address public owner;
-    //cost for having a premium membership
+    //a state variable of the premium price
     uint256 public premiumPrice;
 
-    //struct containing the details of a book and if its a premium book or not
-    struct Book {
+    //a struct that stores information about the book
+    struct Book{
         string author;
         string title;
         uint256 id;
         bool isPremium;
     }
-    //array of structs that allows the owner of the contract store books
-    Book[] public books;
-    //a mapping of address to boolean showing whether the owner of the address has a premium plan or not
-    mapping(address => bool) public premiumMembers;
 
-    //constructor that allows the owner of the contract set a price for the premium plan and sets the owner to the person that deploys the contract
-    constructor(uint256 _premiumPrice) {
+    //an array of struct for the books to be stored in
+    Book[] public books;
+
+    //a mapping that shows if a user is a premium member
+    mapping(address => bool) public premiumUser;
+
+    //a constructor that allows the owner of the contract to set the premium price
+    constructor(uint256 _premiumPrice){
         owner = msg.sender;
         premiumPrice = _premiumPrice;
     }
 
-    //a modifier that restricts some functions to be called by only the owner
-    modifier onlyOwner() {
+    //a function modifier that requires only the owner to perform some actions
+    modifier onlyOwner(){
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
     }
-    //a modifier that restricts some functions to be called by only users with premium plan
-    modifier onlyPremium() {
-        require(premiumMembers[msg.sender], "Only premium members can perform this action");
+    //a function modifier that allows only premium users perform an action
+    modifier onlyPremium(){
+        require(premiumUser[msg.sender], "Only premium users can perform this action, buy premium plan");
         _;
     }
 
-    //function that allows the owner of the contract add books to the array whether premiuim books or not
-    function addBook(string memory _author, string memory _title, uint256 _id, bool _isPremium) public onlyOwner {
+    //a function that allows the owner of the contract push books to the array
+    function addBooks(string memory _author, string memory _title, uint256 _id, bool _isPremium) public onlyOwner{
         books.push(Book(_author, _title, _id, _isPremium));
     }
 
-    //function that allows users purchase a premium membership by sending the amount of money specified as the premiumPrice
-    function buyPremium() public payable {
-        require(msg.value == premiumPrice, "Incorrect payment amount");
-        premiumMembers[msg.sender] = true;
+    //a function that allows users purchase a premium membership
+    function buyPremium() public payable{
+        require(msg.value == premiumPrice, "Incorrect price");
+        premiumUser[msg.sender] = true;
     }
 
-    //an event that lets the front end know the address of the user that called the getBook function, the details of the book they want to access and if they are premium users or not
-    event bookAccessed(address indexed user, string author, string title, bool isPremium, uint256 id);
+    //an event that lets the front end know the address of the person getting the book and the details of the book
+    //indexed is used in events and it allows parameters to be included in the ethereum logs topic
+    event bookAccessed(address indexed user, string author, string title, uint256 id, bool isPremium);
 
-    //allows user access the details of a book
-    function getBook(uint256 _index) public payable returns (string memory author, string memory title, uint256 id, bool isPremium) {
+    //a function that allows the user buy a book and allows only premium members buy premium books, also allows user view the book they are buying
+    function getBook(uint _index) public payable returns(string memory author, string memory title, uint256 id, bool isPremium){
+        //a new variable that will contain the details of the book and that will be stored in storage
         Book storage book = books[_index];
-        emit bookAccessed(msg.sender, book.author, book.title, book.isPremium, book.id);
-        if (book.isPremium) {
-            require(premiumMembers[msg.sender], "This book is only available for premium members");
+        //emit the event
+        emit bookAccessed(msg.sender, book.author, book.title, book.id, book.isPremium);
+        //allow only premium users access premium books
+        if(book.isPremium){
+            require(premiumUser[msg.sender], "Only premium users have access to this book, buy premium plan");
         }
-        return (book.author, book.title, book.id, book.isPremium);
-      
-    }
-   
-    //allows the owner of the contract to withdraw the money paid by premium users
-    function withdraw() public onlyOwner {
-        payable(owner).transfer(address(this).balance);
+        return(book.author, book.title, book.id, book.isPremium);
     }
 
-    //allows the owner of the contract to change the premium price
-    function setNewPremiumPrice(uint256 _newPrice) public onlyOwner {
+    //a function that allows the owner of the contract set a new premium price
+    function newPremiumPrice(uint256 _newPrice) public onlyOwner{
         premiumPrice = _newPrice;
+    }
+
+    //function that allows owner of contract withdraw all the money paid by premium users
+    function withdraw() public onlyOwner{
+        payable(owner).transfer(address(this).balance);
     }
 }
